da82cbfea748b714c70dcb41a9a6d2446e8a3f75
SuyogKhanal5 <suyogkhanal@outlook.com>
2021-10-14 22:14:39 -0400

    Added About Me Page

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, redirect
 
 views = Blueprint("views", __name__)
 
@@ -10,3 +10,15 @@ def home():
 @views.route("/updates")
 def updates():
     return render_template("updates.html")
+
+@views.route("/suyog")
+def suyog():
+    return render_template("suyog.html")
+
+@views.route("twitter")
+def twitter():
+    return redirect("https://twitter.com/SuyogKhanal555")
+
+@views.route("github")
+def github():
+    return redirect("https://github.com/SuyogKhanal5")
